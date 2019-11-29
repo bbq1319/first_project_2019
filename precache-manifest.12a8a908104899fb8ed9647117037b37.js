0b8d5e4d32c5c730e161ad70885656529a47bedb
bbq1319 <qhshef@gmail.com>
2019-11-30 01:23:22 +0900

    Updates

---
+++ precache-manifest.12a8a908104899fb8ed9647117037b37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f1a58b0d4a67d1aca235f0bb1f2b277",
+    "url": "/first_project_2019/index.html"
+  },
+  {
+    "revision": "3bae74b5a067d513175f",
+    "url": "/first_project_2019/static/css/main.82b81385.chunk.css"
+  },
+  {
+    "revision": "d89c02e86c678db07344",
+    "url": "/first_project_2019/static/js/2.3cd7aa4b.chunk.js"
+  },
+  {
+    "revision": "3bae74b5a067d513175f",
+    "url": "/first_project_2019/static/js/main.64c0a0b6.chunk.js"
+  },
+  {
+    "revision": "5fe04bcfcdcbc6257355",
+    "url": "/first_project_2019/static/js/runtime-main.0de7f553.js"
+  }
+]);
